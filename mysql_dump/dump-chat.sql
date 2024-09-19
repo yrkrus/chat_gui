90bd11a55ce56a1dc61df9851b0908db07801fa8
yrkrus <137649829+yrkrus@users.noreply.github.com>
2024-09-20 00:27:08 +0300

    20240920_0027

--- mysql_dump/dump-chat.sql
+++ mysql_dump/dump-chat.sql
@@ -38,7 +38,7 @@ CREATE TABLE `message` (
   `is_view` tinyint(1) DEFAULT '0' COMMENT 'сообщение просмотрено',
   `msg` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (47,8,-1,'2024-09-17 18:59:09',0,'Hello World!'),(48,8,-1,'2024-09-17 19:00:02',0,'Тестовое сообщение'),(51,6,-1,'2024-09-17 19:17:01',0,'тут что то написано'),(52,6,-1,'2024-09-17 20:38:43',0,'eeer  tr e'),(53,6,-1,'2024-09-18 09:33:48',0,'ertyty');
+INSERT INTO `message` VALUES (47,8,-1,'2024-09-17 18:59:09',0,'Hello World!'),(48,8,-1,'2024-09-17 19:00:02',0,'Тестовое сообщение'),(51,9,-1,'2024-09-17 19:17:01',0,'Тестовое сообщение 2');
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -115,7 +115,7 @@ CREATE TABLE `users` (
   `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'время последнего входа',
   `state` int(11) NOT NULL COMMENT 'статус(активен\\неактивен)',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,7 +124,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (6,'test','4781474082476124557','Тест','Тестович','2024-09-18 09:33:36',1),(8,'test2','14192856571102425018','Тест2','Тестович2','2024-09-17 21:11:16',1);
+INSERT INTO `users` VALUES (6,'test','4781474082476124557','Тест','Тестович','2024-09-19 21:17:22',1),(8,'test2','14192856571102425018','Тест2','Тестович2','2024-09-19 19:36:54',2),(9,'test3','9445970670855370860','Тест3','Тестович3','2024-09-19 19:47:10',3);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,4 +142,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-18 13:02:19
+-- Dump completed on 2024-09-20  0:26:08
